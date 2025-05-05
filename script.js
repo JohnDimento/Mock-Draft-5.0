97adb738d7c8034beaeda9c2a60902c3b3d5b15e
John Dimento <jkdmnto1@memphis.edu>
2025-05-05 18:32:32 -0500

    Updated code to fic players not showing up

--- script.js
+++ script.js
@@ -2,53 +2,43 @@
 // Last updated: May 5, 2024
 const draftData = {
     round1: [
-        { pick: 1, team: "Winston's Warriors", player: "Ashton Jeanty", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/chi.png" },
-        { pick: 2, team: "Winston's Warriors", player: "Tetairoa Mcmillan", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/wsh.png" },
-        { pick: 3, team: "DoosBoys", player: "Omarion Hampton", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 4, team: "Dropkick Murphys", player: "Matthew Golden", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 5, team: "Lil Peanut's Padres", player: "Travis Hunter", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 6, team: "Watauga Whonsters", player: "Quinshon Judkins", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 7, team: "Memphis Jets", player: "Cam Ward", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 8, team: "Dropkick Murphys", player: "Treveyon Henderson", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 9, team: "Winston's Warriors", player: "Colston Loveland", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 10, team: "Highland Hawgs", player: "Kaleb Johnson", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 11, team: "Winston's Warriors", player: "Emeka Egbuka", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 12, team: "Kilpatty Vegas", player: "Luther Burden", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },   
-        
-        // Add more picks as needed
+        { pick: 1, team: "Winston's Warriors", player: "Ashton Jeanty", position: "RB", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/CHI" },
+        { pick: 2, team: "Winston's Warriors", player: "Tetairoa Mcmillan", position: "WR", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/WAS" },
+        { pick: 3, team: "DoosBoys", player: "Omarion Hampton", position: "RB", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/NE" },
+        { pick: 4, team: "Dropkick Murphys", player: "Matthew Golden", position: "WR", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/ARI" },
+        { pick: 5, team: "Lil Peanut's Padres", player: "Travis Hunter", position: "WR/CB", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/LAC" },
+        { pick: 6, team: "Watauga Whonsters", player: "Quinshon Judkins", position: "RB", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/NYG" },
+        { pick: 7, team: "Memphis Jets", player: "Cam Ward", position: "QB", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/TEN" },
+        { pick: 8, team: "Dropkick Murphys", player: "Treveyon Henderson", position: "RB", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/ATL" },
+        { pick: 9, team: "Winston's Warriors", player: "Colston Loveland", position: "TE", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/CHI" },
+        { pick: 10, team: "Highland Hawgs", player: "Kaleb Johnson", position: "RB", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/NYJ" },
+        { pick: 11, team: "Winston's Warriors", player: "Emeka Egbuka", position: "WR", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/MIN" },
+        { pick: 12, team: "Kilpatty Vegas", player: "Luther Burden", position: "WR", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/DEN" }
     ],
     round2: [
-        { pick: 13, team: "Winston's Warriors", player: "RJ Harvey", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/gb.png" },
-        { pick: 14, team: "Rocky Top Rowdies", player: "Cam Skattebo", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/tb.png" },
-        { pick: 15, team: "Highland Hawgs", player: "Tyler Warren", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ari.png" },
-        { pick: 16, team: "Highland Hawgs", player: "Jaxson Dart", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 17, team: "Lil Peanut's Padres", player: "Tre Harris", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 18, team: "Winston's Warriors", player: "Jayden Higgins", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 19, team: "Winston's Warriors", player: "Brayshul Tuten", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 20, team: "The Dukes", player: "Jack Bech", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 21, team: "Memphis Jets", player: "Mason Taylor", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 22, team: "Expiring Goods", player: "Kyle Williams", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 23, team: "Lil Peanut's Padres", player: "Jaylin Noel", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-        { pick: 24, team: "Rocky Top Rowdies", player: "Pat Bryant", logo: "https://a.espncdn.com/i/teamlogos/nfl/500/ne.png" },
-
-
-
-
-
-
-
-        // Add more picks as needed
+        { pick: 13, team: "Winston's Warriors", player: "RJ Harvey", position: "RB", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/GB" },
+        { pick: 14, team: "Rocky Top Rowdies", player: "Cam Skattebo", position: "RB", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/TB" },
+        { pick: 15, team: "Highland Hawgs", player: "Tyler Warren", position: "TE", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/ARI" },
+        { pick: 16, team: "Highland Hawgs", player: "Jaxson Dart", position: "QB", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/SF" },
+        { pick: 17, team: "Lil Peanut's Padres", player: "Tre Harris", position: "WR", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/BUF" },
+        { pick: 18, team: "Winston's Warriors", player: "Jayden Higgins", position: "WR", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/PHI" },
+        { pick: 19, team: "Winston's Warriors", player: "Brayshul Tuten", position: "RB", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/NO" },
+        { pick: 20, team: "The Dukes", player: "Jack Bech", position: "WR", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/PIT" },
+        { pick: 21, team: "Memphis Jets", player: "Mason Taylor", position: "TE", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/MIA" },
+        { pick: 22, team: "Expiring Goods", player: "Kyle Williams", position: "WR", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/BAL" },
+        { pick: 23, team: "Lil Peanut's Padres", player: "Jaylin Noel", position: "WR", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/HOU" },
+        { pick: 24, team: "Rocky Top Rowdies", player: "Pat Bryant", position: "WR", logo: "https://static.www.nfl.com/t_q-best/league/api/clubs/logos/DAL" }
     ]
 };
 
 function createPickCard(pick) {
     return `
-        <div class="pick-card bg-white rounded-lg p-4 shadow flex items-center space-x-4 hover:shadow-lg">
+        <div class="pick-card">
             <div class="pick-number">${pick.pick}</div>
             <img src="${pick.logo}" alt="${pick.team} logo" class="team-logo">
-            <div class="flex-1">
-                <div class="font-bold text-nfl-blue">${pick.team}</div>
-                <div class="text-gray-600">${pick.player}</div>
+            <div class="pick-details">
+                <div class="team-name">${pick.team}</div>
+                <div class="player-name">${pick.player} - ${pick.position}</div>
             </div>
         </div>
     `;
